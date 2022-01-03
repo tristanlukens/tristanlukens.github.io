b973a5d9901625408b0291d3470ddd75ff2f2123
tristanlukens <71601628+tristanlukens@users.noreply.github.com>
2022-01-03 16:47:16 +0100

    made toggleMenu optional in nav (by giving it a value

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -3,11 +3,13 @@
 
 	// needs a void function with no paramaters because I
 	// could not get a readable funciton store to work
-	export let toggleMenu: () => void;
+
+	// https://stackoverflow.com/questions/62405066/is-there-a-way-to-declare-props-as-optional-in-svelte
+	export let toggleMenu: () => void = undefined;
 </script>
 
 <nav>
-	<ul class="flex flex-col items-center gap-6">
+	<ul class="flex flex-col sm:flex-row items-center gap-6 sm:space-between">
 		{#each navItems as item}
 			<li>
 				<a on:click={toggleMenu} class="link" href="/{item}">{item}</a>

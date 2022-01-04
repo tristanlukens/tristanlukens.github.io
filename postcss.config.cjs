6b83ab0f92941ed22560ca5d3ded995f4c867af8
tristanlukens <71601628+tristanlukens@users.noreply.github.com>
2022-01-04 13:25:10 +0100

    added nesting

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,10 +1,12 @@
+const nesting = require("tailwindcss/nesting");
 const tailwindcss = require("tailwindcss");
 const autoprefixer = require("autoprefixer");
 
 const config = {
 	plugins: [
 		//Some plugins, like tailwindcss/nesting, need to run before Tailwind,
-		tailwindcss(),
+		nesting,
+		tailwindcss,
 		//But others, like autoprefixer, need to run after,
 		autoprefixer,
 	],

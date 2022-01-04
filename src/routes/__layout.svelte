1e87c25476fc65027cb27d367b003c4d823c512b
tristanlukens <71601628+tristanlukens@users.noreply.github.com>
2022-01-04 19:46:24 +0100

    more semantic names in layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,11 +4,11 @@
 	import Header from "../components/header.svelte";
 </script>
 
-<div class="fixed top-0 left-0 right-0 bg-white">
+<header class="fixed top-0 left-0 right-0 bg-white">
 	<Header />
-</div>
+</header>
 
 <!-- mt-12 because 12 is the size of the header -->
-<div class="mt-12">
+<section class="mt-12">
 	<slot />
-</div>
+</section>

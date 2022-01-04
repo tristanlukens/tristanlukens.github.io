437a83f97318b0c3ff8011981226703e30f3228c
tristanlukens <71601628+tristanlukens@users.noreply.github.com>
2022-01-04 16:36:36 +0100

    made article component

---
+++ src/components/article.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import SvelteMarkdown from "svelte-markdown";
+
+	export let source: string;
+	export let author: string;
+	export let date: string;
+</script>
+
+<article>
+	<div class="prose prose-a:text-theme-500 text-justify">
+		<SvelteMarkdown {source} />
+	</div>
+	<hr class="horizontal-break" />
+	<p class="italic leading-loose">Written by {author} on {date}.</p>
+</article>

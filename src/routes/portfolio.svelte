5c116bfbb9ca392990e24c4bce65b5d2d3f1d312
tristanlukens <71601628+tristanlukens@users.noreply.github.com>
2022-01-04 11:18:22 +0100

    added metadata to portfolio and settings pages

---
+++ src/routes/portfolio.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+	<title>tristanlukens | Portfolio</title>
+	<meta
+		name="description"
+		content="Portfolio of Tristan Lukens' personal website."
+	/>
+</svelte:head>
---
+++ src/routes/settings.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+	<title>tristanlukens | Settings</title>
+	<meta
+		name="description"
+		content="Settings page of Tristan Lukens' personal website."
+	/>
+</svelte:head>

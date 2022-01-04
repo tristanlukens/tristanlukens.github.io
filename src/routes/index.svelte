767b9c038ccbe2d20b6e96a6207b7854e1b8fe1c
tristanlukens <71601628+tristanlukens@users.noreply.github.com>
2022-01-04 11:12:10 +0100

    made the home page

---
+++ src/routes/index.svelte
@@ -0,0 +1,16 @@
+<main
+	class="grid height-normal grid-cols-1 grid-rows-2 md:grid-cols-2 md:grid-rows-1 place-items-center text-center p-8"
+>
+	<div class="row-span-1">
+		<h1 class="font-bold text-4xl md:text-5xl lg:text-6xl">
+			tristan<span class="text-theme-400">lukens</span>
+		</h1>
+		<p class="mt-4 text-lg">Welcome.</p>
+	</div>
+	<div class="max-w-[30ch] md:max-w-[40ch] row-span-2">
+		<p class="text-sm sm:text-base md:text-lg md:text-right !leading-loose">
+			I'm <span class="font-bold">Tristan</span>! You're on my personal
+			site. There's not a lot to do here, but for now, have a nice stay.
+		</p>
+	</div>
+</main>

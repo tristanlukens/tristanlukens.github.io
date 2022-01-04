51ca8610ef1e5f3cffdec51d2fa4b886f8a20e41
tristanlukens <71601628+tristanlukens@users.noreply.github.com>
2022-01-04 20:17:58 +0100

    created blog

---
+++ src/components/card.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let title;
+	export let date;
+
+	const link = `/post/${title.split(" ").join("-").toLowerCase()}`;
+</script>
+
+<a
+	href={link}
+	class="block w-full bg-gradient-to-tr from-theme-600 to-theme-400 text-white px-8 py-4 shadow-xl rounded-lg cursor-pointer hover:-translate-y-1 transition"
+>
+	<h4 class="font-bold">{title}</h4>
+	<p class="mt-2 text-xs italic">{date}</p>
+</a>
--- src/routes/blog.svelte
+++ src/routes/blog.svelte
@@ -1,3 +1,14 @@
+<script lang="ts">
+	import Card from "../components/card.svelte";
+
+	const articles = [
+		{
+			title: "This site",
+			date: "04-01-2022",
+		},
+	];
+</script>
+
 <svelte:head>
 	<title>tristanlukens | Blog</title>
 	<meta
@@ -5,3 +16,17 @@
 		content="Blog page of Tristan Lukens' personal website."
 	/>
 </svelte:head>
+
+<main class="page">
+	<h1 class="title">blog</h1>
+	<p class="text-center">I like tech. I write about it here.</p>
+	<p class="mt-4 mb-8 text-xs text-center">
+		But don't be afraid to see posts about other subjects.
+	</p>
+	<h3 class="mb-6 font-bold text-xl">Recommended posts</h3>
+	<section class="space-y-8 w-4/5 max-w-sm">
+		{#each articles as article}
+			<Card {...article} />
+		{/each}
+	</section>
+</main>

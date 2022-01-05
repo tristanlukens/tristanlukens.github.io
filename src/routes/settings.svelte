517cac00a2401c94cbc686ebd9de92aa9fc3ca94
tristanlukens <71601628+tristanlukens@users.noreply.github.com>
2022-01-05 16:26:27 +0100

    created settings page (or did I?)

--- src/routes/settings.svelte
+++ src/routes/settings.svelte
@@ -5,3 +5,16 @@
 		content="Settings page of Tristan Lukens' personal website."
 	/>
 </svelte:head>
+
+<main class="page">
+	<h1 class="title">coming<span>soon</span></h1>
+	<div class="prose">
+		<p>The settings page is not done yet, but it will be soon.</p>
+		<hr />
+		<p>You will be able to</p>
+		<ul>
+			<li>change the theme colour of the site</li>
+			<li>enable dark mode</li>
+		</ul>
+	</div>
+</main>

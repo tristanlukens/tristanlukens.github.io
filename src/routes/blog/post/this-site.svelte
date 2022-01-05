ed1b3bfb492c4de0d3e95f18fc6c277756ae2a48
tristanlukens <71601628+tristanlukens@users.noreply.github.com>
2022-01-05 20:45:08 +0100

    added head

--- src/routes/blog/post/this-site.svelte
+++ src/routes/blog/post/this-site.svelte
@@ -5,4 +5,12 @@
 	const data = $articles[0];
 </script>
 
+<svelte:head>
+	<title>tristanlukens | {data.title}</title>
+	<meta
+		name="description"
+		content="The '{data.title}' blog post by {data.author}."
+	/>
+</svelte:head>
+
 <Article {...data} />

c798cee0ba88d9132877730cc1c6d51661ed3caf
tristanlukens <71601628+tristanlukens@users.noreply.github.com>
2022-01-05 16:41:43 +0100

    added the ideas post

---
+++ src/routes/blog/post/ideas-for-articles.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Article from "$lib/article.svelte";
+	import { articles } from "$lib/../stores.js";
+
+	const data = $articles[0];
+</script>
+
+<svelte:head>
+	<title>tristanlukens | {data.title}</title>
+	<meta
+		name="description"
+		content="The '{data.title}' blog post by {data.author}."
+	/>
+</svelte:head>
+
+<Article {...data} />
--- src/stores.js
+++ src/stores.js
@@ -6,13 +6,25 @@ export let menuOpen = writable(false);
 // store with all articles' data
 export const articles = readable([
 	{
-		title: "This site",
+		title: "Ideas for articles",
 		date: "05-01-2022",
 		author: "Tristan Lukens",
 		source: `
-# This site
+# Ideas for articles
 
-This is a post about this site.
+I have a couple of ideas for articles.
+
+- HTML crash course
+- CSS crash course (also covering SASS)
+- Markdown
+- Git and GitHub
+- TailwindCSS
+- vim
+
+I hope I will cover all of these soon.
+
+Also, in my current about page, I talk various subjects, all of which I could also write
+articles.
 `,
 	},
 ]);

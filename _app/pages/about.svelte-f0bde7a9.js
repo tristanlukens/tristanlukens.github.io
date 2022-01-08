82a8d37af6e80d43b61bf1a82a77fea21dcc1488
Tristan Lukens <71601628+tristanlukens@users.noreply.github.com>
2022-01-08 10:28:55 +0100

    Updates

--- _app/pages/about.svelte-f0bde7a9.js
+++ _app/pages/about.svelte-f0bde7a9.js
@@ -1,4 +1,4 @@
-import{S as x,i as C,s as E,B as N,e as b,j as v,v as I,V as O,c as f,d as l,l as _,a as k,w as S,b as m,D as g,f as T,x as $,y as P,z as W,p as A,n as M,A as j}from"../chunks/vendor-f28c4b56.js";import{T as D}from"../chunks/title-53d80759.js";import{A as z}from"../chunks/article-94fdd433.js";function H(c){let t,d,a,n,p,i,o,h;n=new D({props:{first:"about",second:"me"}});const u=[{aboutme:!0},c[0]];let w={};for(let e=0;e<u.length;e+=1)w=N(w,u[e]);return o=new z({props:w}),{c(){t=b("meta"),d=v(),a=b("main"),I(n.$$.fragment),p=v(),i=b("div"),I(o.$$.fragment),this.h()},l(e){const s=O('[data-svelte="svelte-11mdtje"]',document.head);t=f(s,"META",{name:!0,content:!0}),s.forEach(l),d=_(e),a=f(e,"MAIN",{class:!0});var r=k(a);S(n.$$.fragment,r),p=_(r),i=f(r,"DIV",{class:!0});var y=k(i);S(o.$$.fragment,y),y.forEach(l),r.forEach(l),this.h()},h(){document.title="tristanlukens | About",m(t,"name","description"),m(t,"content","About page of Tristan Lukens' personal website."),m(i,"class","-mt-8"),m(a,"class","page")},m(e,s){g(document.head,t),T(e,d,s),T(e,a,s),$(n,a,null),g(a,p),g(a,i),$(o,i,null),h=!0},p(e,[s]){const r=s&1?P(u,[u[0],W(e[0])]):{};o.$set(r)},i(e){h||(A(n.$$.fragment,e),A(o.$$.fragment,e),h=!0)},o(e){M(n.$$.fragment,e),M(o.$$.fragment,e),h=!1},d(e){l(t),e&&l(d),e&&l(a),j(n),j(o)}}}function L(c){return[{title:void 0,source:`
+import{S as x,i as C,s as E,B as N,e as b,j as v,v as I,V as O,c as f,d as l,l as _,a as k,w as S,b as m,D as g,f as T,x as $,y as P,z as W,p as A,n as M,A as j}from"../chunks/vendor-f28c4b56.js";import{T as z}from"../chunks/title-53d80759.js";import{A as D}from"../chunks/article-94fdd433.js";function H(c){let t,d,a,n,p,i,o,h;n=new z({props:{first:"about",second:"me"}});const u=[{aboutme:!0},c[0]];let w={};for(let e=0;e<u.length;e+=1)w=N(w,u[e]);return o=new D({props:w}),{c(){t=b("meta"),d=v(),a=b("main"),I(n.$$.fragment),p=v(),i=b("div"),I(o.$$.fragment),this.h()},l(e){const s=O('[data-svelte="svelte-11mdtje"]',document.head);t=f(s,"META",{name:!0,content:!0}),s.forEach(l),d=_(e),a=f(e,"MAIN",{class:!0});var r=k(a);S(n.$$.fragment,r),p=_(r),i=f(r,"DIV",{class:!0});var y=k(i);S(o.$$.fragment,y),y.forEach(l),r.forEach(l),this.h()},h(){document.title="tristanlukens | About",m(t,"name","description"),m(t,"content","About page of Tristan Lukens' personal website."),m(i,"class","-mt-8"),m(a,"class","page")},m(e,s){g(document.head,t),T(e,d,s),T(e,a,s),$(n,a,null),g(a,p),g(a,i),$(o,i,null),h=!0},p(e,[s]){const r=s&1?P(u,[u[0],W(e[0])]):{};o.$set(r)},i(e){h||(A(n.$$.fragment,e),A(o.$$.fragment,e),h=!0)},o(e){M(n.$$.fragment,e),M(o.$$.fragment,e),h=!1},d(e){l(t),e&&l(d),e&&l(a),j(n),j(o)}}}function L(c){return[{title:void 0,source:`
 Hi! I'm Tristan, 15 years old and live in the Netherlands (and was also born there).
 
 I like development, especially web development (now Svelte, Tailwind and TS). Go as well, but I haven't done
@@ -13,9 +13,9 @@ a year at the time of writing; I started in November 2020.
 It's been a long time. I started programming in 2017 (I think, could be 2018) 
 with Swift Playgrounds on my iPad mini. I did not have a computer at the time. When I did get one, 
 (a 2011 iMac), I went to Xcode and started to learn iOS app development. Xcode was an absolute 
-disaster back then: it was slow, buggy and genuinly unfun to use. Even when I got my 2021 Mac mini. I quit, 
+disaster back then: it was slow, buggy and genuinely unfun to use. Even when I got my 2021 Mac mini. I quit, 
 and bought a Unity game development course on Udemy in 2019. I should have known however,
-that a computer with a 2012 integrated GPU wouldn't cut it. It was too slow. Duh.
+that a computer with a 2012 integrated GPU wouldn't cut it. It was too slow.
 
 I stopped for a while, but picked up iOS app development again in May(?) of 2020. Still didn't work, moved on
 to Python. Which I couldn't get working: there's two commands: \`python\` and \`python3\`. You
@@ -31,7 +31,7 @@ programming language (Swift)). Sue me. I dare you.
 
 Around about November 2020 I stumbled upon [this](https://www.youtube.com/watch?v=vQWlgd7hV4A&t=8993s) video.
 This did work! HTML is just a little bit of text after all. No compiling
-or installation involved. Just VS Code, which was a rather simple \`brew install visual-studio-code
+or installation involved. Just VS Code, which was a rather simple \`brew install visual-studio-code\`
 because I had played with the terminal before. Besides, I had tinkered with Linux already (and completely
 screwed a system, but that's a story for another blog post).
 

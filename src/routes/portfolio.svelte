0b6da59151afad1ae0f99d471486c28c81c17607
tristanlukens <71601628+tristanlukens@users.noreply.github.com>
2022-03-11 20:56:05 +0100

    removed everything

--- src/routes/portfolio.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-	import Comingsoon from "$lib/comingsoon.svelte";
-</script>
-
-<Comingsoon message="The portfolio page is not done yet." />
--- src/routes/settings.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-	import Comingsoon from "$lib/comingsoon.svelte";
-</script>
-
-<Comingsoon message="The settings page is not done yet." />
